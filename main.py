0ebc85697fdb1676520b9e4e87e59d044da03f60
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2019-12-31 23:14:42 -0800

    Clean out code

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ import math
 from _tkinter import TclError
 
 # Other module imports
-from classdefs import Button
+from classdefs import Button, btns
 
 # Window boilerplate
 wn = turtle.Screen()
-wn.title('Questionable means of Exploration')
+wn.title('Questionable means of exploration')
 wn.setup(height = 800, width = 800)
 wn.bgcolor('#464646'); wn.listen(); wn.tracer(0)
 
 # Exiting
 wn.onkeypress(turtle.bye, 'Escape')
 
-basicButton = Button(0, 200, 5, 3, '#FF0000', '#00FF00', 'Hello, World!')
+def game(*args):
 
-try:
 	while True:
 		wn.update()
 
-		# Need to loop or else gets written over.
-		basicButton.write_text('Hello, World!', '#00FF00')
+		for b in btns:
+			b.write_text()
+
+try:
+	game()
+
 except (TclError, turtle.Terminator):
-	pass
+	pass;
+
+wn.mainloop()
