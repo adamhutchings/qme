9f16e047212c03b1d35277523c7de18db6559ae5
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2019-12-31 23:47:51 -0800

    Add "scrolling" with arrow keys

--- main.py
+++ main.py
@@ -18,6 +18,29 @@ wn.onkeypress(turtle.bye, 'Escape')
 # Tiles
 world = TileField(2, 'water')
 
+# 'Scrolling' in all directions
+def up():
+	for tile in tilesList:
+		tile.t.sety(tile.t.ycor() - 20) # It's minus because only the screen is 'going up'
+
+def down():
+	for tile in tilesList:
+		tile.t.sety(tile.t.ycor() + 20)
+
+def left():
+	for tile in tilesList:
+		tile.t.setx(tile.t.xcor() + 20)
+
+def right():
+	for tile in tilesList:
+		tile.t.setx(tile.t.xcor() - 20)
+
+# Keybinds for scrolling
+wn.onkeypress(up, 'Up')
+wn.onkeypress(down, 'Down')
+wn.onkeypress(left, 'Left')
+wn.onkeypress(right, 'Right')
+
 def game(*args):
 
 	while True:
