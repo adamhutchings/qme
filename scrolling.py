de70216244bde1a4b0c63a94b2c1f44f9c4e1afb
Adam Hutchings <adamhutchings@Adams-MacBook-Pro.local>
2020-01-04 22:13:09 -0800

    Units are now moved by scrolling

--- scrolling.py
+++ scrolling.py
@@ -1,21 +1,36 @@
-from classdefs import tilesDict
+from classdefs import tilesDict, unitsList
 
 # 'Scrolling' in all directions
+
+# The directions are inverted from
+# what one might expect because
+# everything moving down, for
+# example, gives the impression
+# that the screen is moving up.
+
 def up():
 	for tile in tilesDict.values():
-		tile.t.sety(tile.t.ycor() - 20) # It's minus because only the screen is 'going up'
+		tile.t.sety(tile.t.ycor() - 20)
+	for unit in unitsList:
+		unit.t.sety(unit.t.ycor() - 20)
 
 def down():
 	for tile in tilesDict.values():
 		tile.t.sety(tile.t.ycor() + 20)
+	for unit in unitsList:
+		unit.t.sety(unit.t.ycor() + 20)
 
 def left():
 	for tile in tilesDict.values():
 		tile.t.setx(tile.t.xcor() + 20)
+	for unit in unitsList:
+		unit.t.setx(unit.t.xcor() + 20)
 
 def right():
 	for tile in tilesDict.values():
 		tile.t.setx(tile.t.xcor() - 20)
+	for unit in unitsList:
+		unit.t.setx(unit.t.xcor() - 20)
 
 def init_binds(wn):
 	# Keybinds for scrolling
